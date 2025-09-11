9a53d5f634b1c4227c1316737d252b9d07d44a08
José Ronquillo <114964339+JoseRonquillo@users.noreply.github.com>
2025-09-10 23:07:37 -0600

    Modelos Producto y Cliente

--- mi_proyecto/tienda/models.py
+++ mi_proyecto/tienda/models.py
@@ -5,3 +5,19 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+    
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+
